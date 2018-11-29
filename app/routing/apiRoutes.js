61aeac39fe17173972a337efccc7c5028292a0ba
Angela <acsmoot14@gmail.com>
2018-11-28 23:48:40 -0600

    small changes formatting

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,40 +1,40 @@
 //    * A GET route with the url `/api/friends`. 
 //    This will be used to display a JSON of all possible friends.
-  // Displays a single friend, or returns false
-  app.get("/api/friends/:friend", function(req, res) {
-    var chosen = req.params.friend;
-  
-    console.log(chosen);
-  
-    for (var i = 0; i < friends.length; i++) {
-      if (chosen === friends[i].routeName) {
-        return res.json(friends[i]);
-      }
-    }
-  
-    return res.json(false);
-  });
+// Displays a single friend, or returns false
+app.get("/api/friends/:friend", function (req, res) {
+  var chosen = req.params.friend;
 
-   // Displays all friends
-   app.get("/api/friends", function(req, res) {
-    return res.json(data/friends);
-  });
+  console.log(chosen);
+
+  for (var i = 0; i < friends.length; i++) {
+    if (chosen === friends[i].routeName) {
+      return res.json(friends[i]);
+    }
+  }
+
+  return res.json(false);
+});
+
+// Displays all friends
+app.get("/api/friends", function (req, res) {
+  return res.json(data / friends);
+});
 
 //   * A POST routes `/api/friends`. 
 // This will be used to handle incoming survey results. 
 // This route will also be used to handle the compatibility logic.
-app.post("/api/friends", function(req, res) {
-    // req.body hosts is equal to the JSON post sent from the user
-    // This works because of our body parsing middleware
-    var newfriend = req.body;
-  
-    // Using a RegEx Pattern to remove spaces from newCharacter
-    // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
-    newfriend.routeName = newfriend.name.replace(/\s+/g, "").toLowerCase();
-  
-    console.log(newfriend);
-  
-    friends.push(newfriend);
-  
-    res.json(newfriend);
-  });
+app.post("/api/friends", function (req, res) {
+  // req.body hosts is equal to the JSON post sent from the user
+  // This works because of our body parsing middleware
+  var newfriend = req.body;
+
+  // Using a RegEx Pattern to remove spaces from newCharacter
+  // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
+  newfriend.routeName = newfriend.name.replace(/\s+/g, "").toLowerCase();
+
+  console.log(newfriend);
+
+  friends.push(newfriend);
+
+  res.json(newfriend);
+});
--- server.js
+++ server.js
@@ -9,18 +9,13 @@ var path = require("path");
 var app = express();
 var PORT = process.env.PORT || 8080;
 // Setup the Express to handle data parsing
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({
+  extended: true
+}));
 app.use(express.json());
 
-// Routes
+// Starts the server to begin listening
 // =============================================================
-// Basic route that sends the user first to the AJAX Page
-  
- 
-  
-  
-  // Starts the server to begin listening
-  // =============================================================
-  app.listen(PORT, function() {
-    console.log("App listening on PORT " + PORT);
-  });
+app.listen(PORT, function () {
+  console.log("App listening on PORT " + PORT);
+});
